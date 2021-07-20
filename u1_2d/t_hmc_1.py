7082387b5f84d8a5652362c06b8bd7c02d82f36b
Xiao-Yong Jin <xjin@anl.gov>
2021-07-20 11:45:24 -0500

    t_hmc_0/1: pure hmc test for 16^2 and 32^2

---
+++ u1_2d/t_hmc_1.py
@@ -0,0 +1,12 @@
+import tensorflow.keras as tk
+import nthmc, ftr, hmctrain
+import sys
+sys.path.append('../lib')
+import field
+
+conf = nthmc.Conf(nbatch=2048, nepoch=10, nstepEpoch=128, nstepMixing=64, nstepPostTrain=1024, initDt=0.1, stepPerTraj=40)
+nthmc.setup(conf)
+loss = lambda action: nthmc.LossFun(action, cCosDiff=0.0, cTopoDiff=1.0, cForce2=0.0, dHmin=0.0, topoFourierN=1)
+opt = tk.optimizers.Adam(learning_rate=0.001)
+mcmc = lambda: nthmc.Metropolis(conf, nthmc.LeapFrog(conf, nthmc.U1d2(beta=7.0, beta0=2.0, size=(32,32), transform=ftr.Ident())))
+hmctrain.run(conf, mcmc, loss, opt)

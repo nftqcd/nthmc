5ed17a634178c00aefa9043604b2c51d6a6bd122
Xiao-Yong Jin <xjin@anl.gov>
2021-09-14 16:41:58 -0500

    reproducibility checks

---
+++ u1_2d/reprod/t_fthmc_0_iop2.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import tensorflow.keras as tk
+import sys
+sys.path.append('..')
+sys.path.append('../../lib')
+import nthmc, ftr, forcetrain
+import field
+
+conf = nthmc.Conf(nbatch=64, nepoch=1, nstepEpoch=1, nstepMixing=128, nstepPostTrain=0, initDt=0.2, stepPerTraj=10, nthr=10, nthrIop=2)
+nthmc.setup(conf)
+op0 = (((1,2,-1,-2), (1,-2,-1,2)),
+       ((1,1,2,-1,-1,-2), (1,1,-2,-1,-1,2), (1,2,-1,-1,-2,1), (1,-2,-1,-1,2,1)))
+# requires different coefficient bounds:
+# (1,2,-1,-2,1,-2,-1,2)
+# (1,2,-1,-2,1,2,-1,-2)
+# (1,-2,-1,2,1,-2,-1,2)
+op1 = (((2,-1,-2,1), (2,1,-2,-1)),
+       ((2,2,-1,-2,-2,1), (2,2,1,-2,-2,-1), (2,-1,-2,-2,1,2), (2,1,-2,-2,-1,2)))
+fixedP = (1,2,-1,-2)
+fixedR0 = (2,2,1,-2,-2,-1)
+fixedR1 = (1,1,2,-1,-1,-2)
+convP0 = lambda: ftr.PeriodicConv((
+    tk.layers.Conv2D(2, (3,2), activation='gelu', kernel_initializer=tk.initializers.RandomNormal(), bias_initializer=tk.initializers.RandomNormal()),
+))
+convP1 = lambda: ftr.PeriodicConv((
+    tk.layers.Conv2D(2, (2,3), activation='gelu', kernel_initializer=tk.initializers.RandomNormal(), bias_initializer=tk.initializers.RandomNormal()),
+))
+convR = lambda pad: ftr.PeriodicConv((
+    tk.layers.Conv2D(2, (3,3), activation='gelu', kernel_initializer=tk.initializers.RandomNormal(), bias_initializer=tk.initializers.RandomNormal()),
+), pad)
+conv = lambda: ftr.PeriodicConv((
+    tk.layers.Conv2D(2, (3,3), activation='gelu', kernel_initializer=tk.initializers.RandomNormal(), bias_initializer=tk.initializers.RandomNormal()),
+    tk.layers.Conv2D(2, (3,3), activation=None, kernel_initializer=tk.initializers.RandomNormal(), bias_initializer=tk.initializers.RandomNormal()),
+))
+transform = lambda: ftr.TransformChain([
+    ftr.GenericStoutSmear(((0,0),(2,2)), op0, [(fixedP, convP0()), (fixedR0, convR((1,2)))], conv()),
+    ftr.GenericStoutSmear(((0,1),(2,2)), op0, [(fixedP, convP0()), (fixedR0, convR((1,2)))], conv()),
+    ftr.GenericStoutSmear(((1,0),(2,2)), op0, [(fixedP, convP0()), (fixedR0, convR((1,2)))], conv()),
+    ftr.GenericStoutSmear(((1,1),(2,2)), op0, [(fixedP, convP0()), (fixedR0, convR((1,2)))], conv()),
+    ftr.GenericStoutSmear(((0,0),(2,2)), op1, [(fixedP, convP1()), (fixedR1, convR((2,1)))], conv()),
+    ftr.GenericStoutSmear(((1,0),(2,2)), op1, [(fixedP, convP1()), (fixedR1, convR((2,1)))], conv()),
+    ftr.GenericStoutSmear(((0,1),(2,2)), op1, [(fixedP, convP1()), (fixedR1, convR((2,1)))], conv()),
+    ftr.GenericStoutSmear(((1,1),(2,2)), op1, [(fixedP, convP1()), (fixedR1, convR((2,1)))], conv()),
+])
+ftr.checkDep(transform())
+rng = tf.random.Generator.from_seed(conf.seed)
+actionFun = lambda: nthmc.U1d2(beta=7.0, beta0=2.0, size=(16,16), transform=transform(), nbatch=conf.nbatch, rng=rng.split()[0])
+loss = lambda action: forcetrain.LossFun(action, betaMap=2.5, cNorm2=1.0, cNormInf=1.0)
+opt = tk.optimizers.Adam(learning_rate=0.001)
+mcmc = lambda action: nthmc.Metropolis(conf, nthmc.LeapFrog(conf, action))
+x, m, l = forcetrain.run(conf, actionFun, mcmc, loss, opt)
+tf.print('MCMC weights:', m.get_weights(), summarize=-1)
+tf.print('opt variables:', len(opt.variables()), opt.variables(), summarize=-1)
